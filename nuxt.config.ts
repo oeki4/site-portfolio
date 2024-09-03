8383b26fa691d2d8833f009fe776da9beaaa0c34
Nikita-Minov <nikitachryackhov555@gmail.com>
2024-09-03 12:39:33 +0300

    Added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      link: [
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "https://oeki.ru/favicon-120.svg",
+          sizes: "32x32",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   modules: ["vue3-carousel-nuxt"],
   runtimeConfig: {
